5b79fd667efe710738cd9a21fd0c7b5f06b88f08
chidakiyo <chidakiyo@gmail.com>
2017-06-12 17:37:36 +0900

    GET Request 処理実装

--- gb.go
+++ gb.go
@@ -4,25 +4,16 @@ import (
 	"fmt"
 	"github.com/codegangsta/cli"
 	"os"
+
+	"go.uber.org/ratelimit"
+	"time"
+	"net/http"
+	"io/ioutil"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "gb"
-	app.Usage = "Usage: gb [options] [http[s]://]hostname[:port]/path"
-	app.Version = "0.0.1"
-	app.HideHelp = true
 
-	app.Flags = []cli.Flag{
-		cli.Int64Flag{
-			Name:  "requests, n",
-			Usage: "Number of requests to perform",
-		},
-		cli.Int64Flag{
-			Name:  "concurrency, c",
-			Usage: "Number of multiple requests to make at a time",
-		},
-	}
+	app := createNewApp()
 
 	app.Action = gbAction
 
@@ -35,6 +26,28 @@ func main() {
 	}
 
 	app.Run(os.Args)
+}
+
+func createNewApp() *cli.App {
+	app := cli.NewApp()
+	app.Name = "gb"
+	app.Usage = "Usage: gb [options] [http[s]://]hostname[:port]/path"
+	app.Version = "0.0.1"
+	app.HideHelp = true
+
+	app.Flags = []cli.Flag{
+		// -n オプション : 総リクエスト数
+		cli.Int64Flag{
+			Name:  "requests, n",
+			Usage: "Number of requests to perform",
+		},
+		// -c オプション : 同時接続数（並列数）
+		cli.Int64Flag{
+			Name:  "concurrency, c",
+			Usage: "Number of multiple requests to make at a time",
+		},
+	}
+	return app
 }
 
 func gbAction(c *cli.Context) {
@@ -52,4 +65,42 @@ func gbAction(c *cli.Context) {
 	fmt.Printf("%d, %d\n", requests, concurrency)
 
 	fmt.Printf("Hello world! %s\n", paramFirst)
+
+	rl := ratelimit.New(2) // per second
+
+	prev := time.Now()
+	for i := 0; i < 100; i++ {
+		now := rl.Take()
+		fmt.Println(i, now.Sub(prev))
+		prev = now
+	}
 }
+
+type RequestResult struct {
+	Body string
+	IsError bool
+	ErrorMsg string
+}
+
+func getRequest(url string, resultChan chan RequestResult) {
+
+	// request
+	res, err := http.Get(url)
+
+	// ハンドリング
+	go func() {
+		b, _ := ioutil.ReadAll(res.Body) // TODO error
+		result := RequestResult{}
+		result.Body = string(b)
+		if err != nil {
+			result.IsError = true
+			result.ErrorMsg = err.Error()
+		}
+		resultChan <- result
+	} ()
+}
+
+
+
+
+
